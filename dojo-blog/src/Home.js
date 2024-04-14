fd714ab5c84cdddaf13ec1c80cc4c41c68273f14
Kassimmm <tawfickkassimmohammed@gmail.com>
2024-04-14 06:00:21 +0000

    complete the dojo blog

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,23 +1,16 @@
-import { useState } from "react";
+import BlogList from "./BlogList";
+import useFetch from "./useFetch";
 
 const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
-    const [blogs, setBlogs] = useState([
-        {title: "Kassimletter", body: "For letter", author: "kassim", id: 1},
-        {title: "schoolfuwad", body: "For school", author: "fuwad", id: 2},
-        {title: "statementjamal", body: "For statement", author: "jamal", id: 3}
-    ])
-
-    return (  
-        <div className="home">
-            <h2>Homepage</h2>
-            
-            {blogs.map((blog) => (
-                    <div className="blogs" key={blog.id}>
-                        <p>{blog.author} with id no. {blog.id} has an info wih title {blog.title} which says {blog.body}</p>
-                    </div>
-                ))}
-        </div>
-    )}
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
  
 export default Home;
--- dojo-blog/src/Navbar.js
+++ dojo-blog/src/Navbar.js
@@ -1,14 +1,19 @@
-const Navbar = () => {
-    return (  
-        <div className="navbar">
-            <h1>The Dojo Blog</h1>
+import { Link } from "react-router-dom";
 
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </div>
-    );
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
 }
  
 export default Navbar;
